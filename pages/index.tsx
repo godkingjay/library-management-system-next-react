b834ff31f342cab215c59dca620bc38ec3a93cde
Jarrian <glorianagojar@gmail.com>
2023-05-19 13:27:47 +0800

    feat: remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { apiConfig } from "@/utils/api";
 import axios from "axios";
-import Link from "next/link";
-import { useState } from "react";
-
 const IndexPage = () => {
 	return <></>;
 };
