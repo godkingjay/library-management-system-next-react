d732a229a42bc12007a55bdb510acbc1897b39aa
Jarrian <glorianagojar@gmail.com>
2023-05-20 09:47:10 +0800

    feat: api.ts to site.ts

---
+++ utils/site.ts
@@ -0,0 +1,7 @@
+export const apiConfig = {
+	apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT as string,
+};
+
+export const jwtConfig = {
+	secretKey: process.env.NEXT_PUBLIC_JWT_SECRET_KEY as string,
+};
