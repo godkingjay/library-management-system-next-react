c83499b267a581665f1ed0d1b924a26c50fe68d0
Jarrian <glorianagojar@gmail.com>
2023-05-20 13:39:05 +0800

    feat: export and add BookCategory model

--- utils/models/book.ts
+++ utils/models/book.ts
@@ -1,12 +1,20 @@
 import { ObjectId } from "mongodb";
 
-interface Book {
+export interface Book {
 	_id: ObjectId;
 	title: string;
 	authorId: string;
 	category: string;
 	ISBN?: string;
 	publicationDate?: Date | string;
+	updatedAt: Date | string;
+	createdAt: Date | string;
+}
+
+export interface BookCategory {
+	_id: ObjectId;
+	name: string;
+	description?: string;
 	updatedAt: Date | string;
 	createdAt: Date | string;
 }
