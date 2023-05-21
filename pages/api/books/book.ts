976f1bf35fe2fc5715fdc8e234a2661fa6e2f67e
Jarrian <glorianagojar@gmail.com>
2023-05-21 14:48:13 +0800

    feat: interface APIEndpointBookParameters

--- pages/api/books/book.ts
+++ pages/api/books/book.ts
@@ -1,10 +1,24 @@
+import bookDb from "@/server/mongo/bookDb";
 import { NextApiRequest, NextApiResponse } from "next";
+
+export interface APIEndpointBookParameters {
+	text: string;
+	string: string;
+	categories: string[];
+	amount: number;
+	available: number;
+	borrowed: number;
+	ISBN: string;
+	publicationDate: Date | string;
+}
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	try {
+		const { booksCollection, bookCategoriesCollection, bookLoansCollection } =
+			await bookDb();
 	} catch (error: any) {
 		return res.status(500).json({
 			statusCode: 500,
