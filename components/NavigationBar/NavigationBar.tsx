66f8399039a7fb7f4554f2595cc2cad64a8a3b91
Jarrian <glorianagojar@gmail.com>
2023-05-21 11:07:10 +0800

    feat: user menu

--- components/NavigationBar/NavigationBar.tsx
+++ components/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "./NavigationBar/Logo";
+import UserMenu from "./NavigationBar/UserMenu";
 
 type NavigationBarProps = {};
 
@@ -9,6 +10,9 @@ const NavigationBar: React.FC<NavigationBarProps> = () => {
 			<div className="flex flex-row w-full sticky top-0 h-[56px] bg-white p-2 items-center justify-center border-b border-gray-200">
 				<div className="w-full h-full flex-1 flex flex-row limit-width gap-x-4">
 					<Logo />
+					<div className="ml-auto">
+						<UserMenu />
+					</div>
 				</div>
 			</div>
 		</>
--- components/NavigationBar/NavigationBar/UserMenu.tsx
+++ components/NavigationBar/NavigationBar/UserMenu.tsx
@@ -1,9 +1,48 @@
+import {
+	Box,
+	Button,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Text,
+} from "@chakra-ui/react";
 import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiChevronDown } from "react-icons/bi";
+import { IoExitOutline } from "react-icons/io5";
 
 type UserMenuProps = {};
 
 const UserMenu: React.FC<UserMenuProps> = () => {
-	return <div>UserMenu</div>;
+	return (
+		<>
+			<Menu>
+				<MenuButton
+					as={Button}
+					rightIcon={<BiChevronDown />}
+				>
+					<Box className="h-4 w-4">
+						<AiOutlineUser className="h-full w-full" />
+					</Box>
+				</MenuButton>
+				<MenuList className="!py-1">
+					<MenuItem
+						className="
+              gap-x-2 !text-red-500
+              hover:bg-red-500 hover:bg-opacity-10
+              focus:bg-red-500 focus:bg-opacity-10
+            "
+					>
+						<Box className="h-6 w-6">
+							<IoExitOutline className="h-full w-full" />
+						</Box>
+						<Text>Sign Out</Text>
+					</MenuItem>
+				</MenuList>
+			</Menu>
+		</>
+	);
 };
 
 export default UserMenu;
