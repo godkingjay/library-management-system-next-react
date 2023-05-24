7c1995bb129627086b500f5aabf5d5c55b6edacd
Jarrian <glorianagojar@gmail.com>
2023-05-24 08:05:29 +0800

    feat: Authors Endpoint return currentPage and totalPage

--- pages/manage/authors.tsx
+++ pages/manage/authors.tsx
@@ -1,5 +1,6 @@
 import Pagination from "@/components/Table/Pagination";
 import { Author } from "@/utils/models/author";
+import { apiConfig } from "@/utils/site";
 import {
 	TableContainer,
 	Table,
@@ -10,26 +11,74 @@ import {
 	Td,
 	Box,
 } from "@chakra-ui/react";
+import axios from "axios";
 import moment from "moment";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { APIEndpointAuthorsParameters } from "../api/authors";
+import useUser from "@/hooks/useUser";
 
 type ManageAuthorsPageProps = {};
 
 const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
-	const [currentPage, setCurrentPage] = useState(1);
+	const { usersStateValue } = useUser();
+
+	const [cPage, setCPage] = useState(1);
+	const [tPages, setTPages] = useState(1);
 	const [tableData, setTableData] = useState<Author[]>([]);
+	const [fetchingData, setFetchingData] = useState(false);
 
 	const itemsPerPage = 10;
-	const pageCount = Math.ceil(tableData.length / itemsPerPage);
 
-	const offset = (currentPage - 1) * itemsPerPage;
+	const offset = (cPage - 1) * itemsPerPage;
 	const currentPageData = tableData.slice(offset, offset + itemsPerPage);
 
-	const handlePageChange = (pageNumber: number) => {
-		setCurrentPage(pageNumber);
-	};
+	const fetchAuthors = useCallback(async (page: number) => {
+		try {
+			if (!fetchingData) {
+				setFetchingData(true);
 
-	useEffect(() => {}, []);
+				const {
+					authors,
+					totalPages,
+				}: {
+					authors: Author[];
+					currentPage: number;
+					totalPages: number;
+				} = await axios
+					.get(apiConfig.apiEndpoint + "/authors/", {
+						params: {
+							apiKey: usersStateValue.currentUser?.auth?.keys[0].key,
+							page: page,
+							fromName:
+								tableData.length > 0
+									? tableData[tableData.length - 1].name
+									: undefined,
+							limit: itemsPerPage,
+						} as APIEndpointAuthorsParameters,
+					})
+					.then((response) => response.data);
+
+				if (authors.length) {
+					setTableData(authors);
+					setTPages(totalPages);
+				}
+
+				setFetchingData(false);
+			}
+		} catch (error: any) {
+			console.error(`=>API: Fetch Authors Failed:\n${error}`);
+			setFetchingData(false);
+		}
+	}, []);
+
+	const handlePageChange = useCallback(async (page: number) => {
+		setCPage(page);
+		await fetchAuthors(page);
+	}, []);
+
+	useEffect(() => {
+		fetchAuthors(cPage);
+	}, []);
 
 	return (
 		<>
@@ -67,9 +116,9 @@ const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
 							<Tbody>
 								{currentPageData.map((item, index) => (
 									<Tr key={index}>
-										<Td>{item.name}</Td>
-										<Td>{item.biography}</Td>
-										<Td>
+										<Td className="text-sm">{item.name}</Td>
+										<Td className="text-sm">{item.biography}</Td>
+										<Td className="text-sm">
 											{item.birthdate
 												? typeof item.birthdate === "string"
 													? moment(item.birthdate).format("DD/MM/YYYY")
@@ -78,7 +127,7 @@ const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
 													  ).format("DD/MM/YYYY")
 												: "---"}
 										</Td>
-										<Td>
+										<Td className="text-sm">
 											{item.updatedAt
 												? typeof item.updatedAt === "string"
 													? moment(item.updatedAt).format("DD/MM/YYYY")
@@ -87,7 +136,7 @@ const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
 													  ).format("DD/MM/YYYY")
 												: "---"}
 										</Td>
-										<Td>
+										<Td className="text-sm">
 											{item.createdAt
 												? typeof item.createdAt === "string"
 													? moment(item.createdAt).format("DD/MM/YYYY")
@@ -96,7 +145,7 @@ const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
 													  ).format("DD/MM/YYYY")
 												: "---"}
 										</Td>
-										<Td>Action</Td>
+										<Td className="text-sm">Action</Td>
 									</Tr>
 								))}
 								{currentPageData.length === 0 && (
@@ -117,8 +166,8 @@ const ManageAuthorsPage: React.FC<ManageAuthorsPageProps> = () => {
 					</TableContainer>
 					<div className="w-full flex flex-col items-center">
 						<Pagination
-							currentPage={currentPage}
-							totalPages={pageCount > 1 ? pageCount : 1}
+							currentPage={cPage}
+							totalPages={tPages > 1 ? tPages : 1}
 							onPageChange={handlePageChange}
 						/>
 					</div>
