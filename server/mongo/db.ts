831cf7eedecadf9e7bb6beb8d93627c778d4f575
Jarrian <glorianagojar@gmail.com>
2023-05-25 07:13:37 +0800

    feat: add session transaction

--- server/mongo/db.ts
+++ server/mongo/db.ts
@@ -3,14 +3,32 @@ import clientPromise from "../mongodb";
 export default async function mongoDb() {
 	const client = await clientPromise;
 	const libraryDb = await client.db("library-db");
+	const session = client.startSession();
 
 	const clientCloseConnection = async () => {
 		await client.close();
 	};
 
+	// const startTransaction = async () => {
+	// 	await session.startTransaction();
+	// };
+
+	// const commitTransaction = async () => {
+	// 	await session.commitTransaction();
+	// };
+
+	// const abortTransaction = async () => {
+	// 	await session.abortTransaction();
+	// };
+
+	// const endSession = async () => {
+	// 	await session.endSession();
+	// };
+
 	return {
 		client,
 		libraryDb,
 		clientCloseConnection,
+		session,
 	};
 }
