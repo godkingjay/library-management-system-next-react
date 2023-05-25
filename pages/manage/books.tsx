e4f494b020867dde51508fd746728dc1a224f6bc
Jarrian <glorianagojar@gmail.com>
2023-05-25 12:06:25 +0800

    feat: ManageBooks Page

--- pages/manage/books.tsx
+++ pages/manage/books.tsx
@@ -1,13 +1,65 @@
 import ManageBreadcrumb from "@/components/Breadcrumb/ManageBreadcrumb";
 import SearchBar from "@/components/Input/Searchbar";
+import useAuth from "@/hooks/useAuth";
+import { ImageOrVideoType } from "@/hooks/useInput";
+import useUser from "@/hooks/useUser";
+import { Book } from "@/utils/models/book";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
 type ManageBooksPageProps = {};
 
+export type BooksModalTypes = "" | "add" | "edit" | "delete";
+
 const ManageBooksPage: React.FC<ManageBooksPageProps> = () => {
 	const [searchText, setSearchText] = useState("");
+
+	const { loadingUser } = useAuth();
+	const { usersStateValue } = useUser();
+
+	const [cPage, setCPage] = useState(1);
+	const [tPages, setTPages] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(10);
+	const [booksData, setBooksData] = useState<Book[]>([]);
+
+	const [fetchingData, setFetchingData] = useState(false);
+	const [submitting, setSubmitting] = useState(false);
+	const [updating, setUpdating] = useState(false);
+	const [deleting, setDeleting] = useState(false);
+
+	const [booksModalOpen, setBooksModalOpen] = useState<BooksModalTypes>("");
+
+	const [bookForm, setBookForm] = useState<
+		Pick<
+			Book,
+			| "title"
+			| "description"
+			| "amount"
+			| "ISBN"
+			| "available"
+			| "authorId"
+			| "borrowedTimes"
+			| "publicationDate"
+			| "categories"
+		> & {
+			cover: ImageOrVideoType | null;
+		}
+	>({
+		title: "",
+		description: "",
+		amount: 0,
+		ISBN: "",
+		available: 0,
+		authorId: "",
+		borrowedTimes: 0,
+		publicationDate: "",
+		categories: [],
+		cover: null,
+	});
+
+	const booksMounted = useRef(false);
 
 	const searchTextChangeHandler = (book: string) => {
 		setSearchText(book);
@@ -45,6 +97,19 @@ const ManageBooksPage: React.FC<ManageBooksPageProps> = () => {
 					</Flex>
 					<Flex className="flex flex-col gap-y-4 shadow-page-box-1 bg-white rounded-lg p-4">
 						<SearchBar onSearch={searchTextChangeHandler} />
+						<Flex
+							direction="row"
+							justifyContent={"end"}
+						>
+							<Button
+								leftIcon={<AiOutlinePlus />}
+								colorScheme="whatsapp"
+								variant="solid"
+								// onClick={() => handleAuthorsModalOpen("add")}
+							>
+								Add Book
+							</Button>
+						</Flex>
 					</Flex>
 				</Box>
 			</Box>
